7cc9ef6777cad369f7e560cbc354120b8649bf5b
s4ng <zalcls951201@gmail.com>
2022-06-05 22:49:56 +0900

    kotlin spring

--- spring-kotlin/src/main/kotlin/com/s4ng/springkotlin/HelloController.kt
+++ spring-kotlin/src/main/kotlin/com/s4ng/springkotlin/HelloController.kt
@@ -1,4 +1,13 @@
 package com.s4ng.springkotlin
 
-class HelloController {
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController(private val helloService: HelloService) { // <- 클래스에 바로 생성자 함수 선언 후, 인자로 주입받음.
+                                                                // 만약 주입받을 빈이 여러개라면 @Authwired를 추가해야 함.
+    @GetMapping("/hello")
+    fun greeting(): String{
+        return helloService.greeting();
+    }
 }
--- spring-kotlin/src/main/kotlin/com/s4ng/springkotlin/HelloService.kt
+++ spring-kotlin/src/main/kotlin/com/s4ng/springkotlin/HelloService.kt
@@ -1,4 +1,10 @@
 package com.s4ng.springkotlin
 
+import org.springframework.stereotype.Service
+
+@Service
 class HelloService {
+    fun greeting(): String {
+        return "hello world!"
+    }
 }
